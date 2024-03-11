530396815a1aa2f42475109ceeaef4dbd9725ca1
Felipe Simari <felipe_simari@hotmail.com>
2024-03-11 16:21:49 -0300

    Criando a contagem regressiva data correta

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Jul 22, 2023 19:00:00");
+const dataDoEvento = new Date("Jul 24, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function(){
